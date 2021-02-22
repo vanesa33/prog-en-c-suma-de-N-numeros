d540668bb7a4c68480ad4c12ba4fe6fb3747aa29
Vanidad <57409926+vanesa33@users.noreply.github.com>
2021-02-22 16:30:35 -0300

    Add files via upload

---
+++ sumaDENumeros/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+namespace SUMA_DE_N_NÚMEROS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte CAN, K;
+            int NUM;
+            int SUM = 0;
+            string linea;
+            Console.Write("LÍMITE:"); linea = Console.ReadLine();
+            CAN = byte.Parse(linea);
+            for (K = 1; K <= CAN; K++)
+            {
+                Console.Write("DIGITE UN NÚMERO:"); linea = Console.ReadLine();
+                NUM = int.Parse(linea);
+                SUM += NUM;
+            }
+            Console.WriteLine("SUMA TOTAL ES : " + SUM);
+            Console.WriteLine("MEDIA ARITMÉTICA: " + SUM / CAN);
+            Console.Write("Pulse una Tecla:"); Console.ReadLine();
+        }
+    }
+}
